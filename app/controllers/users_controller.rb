99ce6221057144c5aa5cea0b87a11024cb5c7c85
Ben Mangelsen <bmangelsen@gmail.com>
2016-12-05 08:37:48 -0500

    Added validation on user model to prevent exception when saving user with duplicate username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save!
+    if @user.save
       session[:current_user_id] = @user.id
       # UserMailer.sign_up_email(@user).deliver_now
       redirect_to posts_path(user: @user.id), notice: "Account successfully created!"
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :email, presence: true
   has_many :posts
+  validates_uniqueness_of :username
   has_secure_password
 end
