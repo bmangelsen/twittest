6d96df75f1f03fdbab1b860faecf322a612405a8
Ben Mangelsen <bmangelsen@gmail.com>
2016-12-05 08:45:49 -0500

    Added rescue for CanCan::AccessDenied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
   def current_user
      @_current_user ||= User.find_by(id: session["current_user_id"])
   end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, :alert => exception.message
+  end
 end
